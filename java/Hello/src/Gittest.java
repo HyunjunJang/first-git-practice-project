29ca235d920319924eba5f7b337363565c345d0a
HyunjunJang <hyunjun301@gmail.com>
2024-01-24 21:20:40 +0900

    third commit

--- java/Hello/src/Gittest.java
+++ java/Hello/src/Gittest.java
@@ -2,5 +2,7 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println(1+1);
         System.out.println(2+3);
+        System.out.println(10*9);
+        System.out.println(10*4);
     }
 }
