dae694f6204250e2d264f6a3e25a5cdd5b3b1fa0
HyunjunJang <hyunjun301@gmail.com>
2024-01-24 21:35:58 +0900

    fifth commit

--- java/Hello/src/Gittest.java
+++ java/Hello/src/Gittest.java
@@ -5,6 +5,7 @@ public class Gittest {
         System.out.println(10*9);
         System.out.println(10*4);
         System.out.println("이번에 git github 마스터하자");
-        
+        System.out.println("파일 위치를 바꾼 등록은 어떻게 할까? 내 생각엔 위치 부분에 git init 을 먼저 실행해야할거같다");
+        System.out.println("이렇게 다시 해보자");
     }
 }
